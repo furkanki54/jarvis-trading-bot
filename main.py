bea03f394393422817e23853f726d94f634acdd5
furkanki54 <132562462+furkanki54@users.noreply.github.com>
2025-06-03 02:19:02 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import time
-print("Jarvis bot hala çalışıyor...")
-while True:
-    time.sleep(60)
+
+print("Jarvis bot başlatıldı...")
+
+try:
+    while True:
+        print("Bot aktif...")  # Her döngüde log'a bir mesaj yaz
+        time.sleep(30)         # 30 saniyede bir çalıştığını gösterir
+except Exception as e:
+    print("Bir hata oluştu:", e)
