990a4d1dd3b15ea0cb54fcfc8763c6a45fd51639
furkanki54 <132562462+furkanki54@users.noreply.github.com>
2025-06-05 15:12:55 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,11 @@
-TELEGRAM_TOKEN = "7759276451:AAF0Xphio-TjtYyFIzahQrG3fU-qdNQuBEw"
-TELEGRAM_CHAT_ID = "-1002549376225"
+# config.py
 
+# Telegram bot ayarları
+TELEGRAM_TOKEN = "7759276451:AAF0Xphio-TjtYyFIzahQrG3fU-qdNQuBEw"
+CHAT_ID = "-1002549376225"
+
+# Binance API ana URL
 BINANCE_API_BASE = "https://api.binance.com"
+
+# Takip edilecek coin listesi dosya adı
 COIN_LIST_FILE = "coin_list_binance.txt"
